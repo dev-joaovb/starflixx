76e6a0d19a994d9b91ea0f856b035de65990914c
João Victor Bueno Carneiro <jvbueno12.jobo@hotmail.com>
2023-10-06 01:07:18 -0300

    correção de erros

--- filme/apps.py
+++ filme/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
-
+    
     def ready(self):
         from .models import Usuario
         import os
--- starflix/settings.py
+++ starflix/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,7 +100,7 @@ DATABASES = {
 }
 ## Responsavel pela criacao do Banco de Dados
 import dj_database_url
-
+import os
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 if DATABASE_URL:
