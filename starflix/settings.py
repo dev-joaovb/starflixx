115f50a3258612572781b3186d1952c8249a2771
João Victor Bueno Carneiro <jvbueno12.jobo@hotmail.com>
2023-10-07 17:55:42 -0300

    correção de erros 12

--- starflix/settings.py
+++ starflix/settings.py
@@ -30,7 +30,7 @@ else:
 ## Chave de seguranca
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ## Sempre verdadeiro
 
 ALLOWED_HOSTS = ['starflixx.up.railway.app', 'localhost', '127.0 0.1']
